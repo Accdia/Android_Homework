0e260352025f4d3a51bd1de013c6452eed9e2278
Accdia <accidia12139@outlook.com>
2023-02-19 09:06:42 +0800

    init

---
+++ Test3/app/src/main/java/com/example/test3/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.test3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int offset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button addButton = (Button) findViewById(R.id.add);
+        Button subButton = (Button) findViewById(R.id.sub);
+        Button mulButton = (Button) findViewById(R.id.mul);
+        Button divButton = (Button) findViewById(R.id.div);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumber = (EditText) findViewById(R.id.num_1);
+                EditText secondNumber = (EditText) findViewById(R.id.num_2);
+                TextView result = (TextView) findViewById(R.id.result);
+
+                try {
+                    double num1 = Double.parseDouble(firstNumber.getText().toString());
+                    double num2 = Double.parseDouble(secondNumber.getText().toString());
+                    double sum = num1 + num2;
+                    String msg = Double.toString(num1) + " + " + Double.toString(num2) + " = " + Double.toString(sum) + "\n";
+                    result.append(msg);
+                    offset=result.getLineCount()*result.getLineHeight();
+                    if(offset>result.getHeight()){
+                        result.scrollTo(0,offset-result.getHeight());
+                    }
+                } catch (NumberFormatException e) {
+                    result.append("Please enter number\n");
+                }
+            }
+        });
+
+        subButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumber = (EditText) findViewById(R.id.num_1);
+                EditText secondNumber = (EditText) findViewById(R.id.num_2);
+                TextView result = (TextView) findViewById(R.id.result);
+
+                try {
+                    double num1 = Double.parseDouble(firstNumber.getText().toString());
+                    double num2 = Double.parseDouble(secondNumber.getText().toString());
+                    double sub = num1 - num2;
+                    String msg = Double.toString(num1) + " - " + Double.toString(num2) + " = " + Double.toString(sub) + "\n";
+
+                    result.append(msg);
+                    offset=result.getLineCount()*result.getLineHeight();
+                    if(offset>result.getHeight()){
+                        result.scrollTo(0,offset-result.getHeight());
+                    }
+                } catch (NumberFormatException e) {
+                    result.append("Please enter number\n");
+                }
+            }
+        });
+
+        mulButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumber = (EditText) findViewById(R.id.num_1);
+                EditText secondNumber = (EditText) findViewById(R.id.num_2);
+                TextView result = (TextView) findViewById(R.id.result);
+
+                try {
+                    double num1 = Double.parseDouble(firstNumber.getText().toString());
+                    double num2 = Double.parseDouble(secondNumber.getText().toString());
+                    double mul = num1 * num2;
+                    String msg = Double.toString(num1) + " * " + Double.toString(num2) + " = " + Double.toString(mul) + "\n";
+
+                    result.append(msg);
+                    offset=result.getLineCount()*result.getLineHeight();
+                    if(offset>result.getHeight()){
+                        result.scrollTo(0,offset-result.getHeight());
+                    }
+                } catch (NumberFormatException e) {
+                    result.append("Please enter number\n");
+                }
+            }
+        });
+
+        divButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumber = (EditText) findViewById(R.id.num_1);
+                EditText secondNumber = (EditText) findViewById(R.id.num_2);
+                TextView result = (TextView) findViewById(R.id.result);
+
+                try {
+                    double num1 = Double.parseDouble(firstNumber.getText().toString());
+                    double num2 = Double.parseDouble(secondNumber.getText().toString());
+                    double div = num1 / num2;
+                    String msg = Double.toString(num1) + " / " + Double.toString(num2) + " = " + Double.toString(div) + "\n";
+
+                    result.append(msg);
+                    offset=result.getLineCount()*result.getLineHeight();
+                    if(offset>result.getHeight()){
+                        result.scrollTo(0,offset-result.getHeight());
+                    }
+                } catch (NumberFormatException e) {
+                    result.append("Please enter number\3");
+                }
+            }
+        });
+    }
+}
